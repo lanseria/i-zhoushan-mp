3cafadd122c8dbdd97e33b171b55f710e34cb7a9
lanseria <zhangchao564265135@hotmail.com>
2022-11-04 21:01:36 +0800

    修复接口地址

--- utils/constant.ts
+++ utils/constant.ts
@@ -1,15 +1,15 @@
-const { baseUrl } = require("../env")
-let host = 'https://8080.vip.cpolar.top';
+const { baseUrl, releaseBaseUrl } = require("../env")
+let host = ''
 // 获取小程序当前版本信息 https://developers.weixin.qq.com/miniprogram/dev/api/open-api/account-info/wx.getAccountInfoSync.html
 // 自动根据版本切换接口请求地址
 const { miniProgram: { envVersion } } = wx.getAccountInfoSync();
 
 console.log(envVersion, 'envVersion');
 if (envVersion === 'release') {
-  host = 'https://8080.vip.cpolar.top';
+  host = releaseBaseUrl;
 }
 if (envVersion === 'trial') {
-  host = 'https://8080.vip.cpolar.top';
+  host = releaseBaseUrl;
 }
 if (envVersion === 'develop') {
   host = baseUrl;
